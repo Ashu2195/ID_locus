ccb209560c8fe1874ee5cb54026bf186c0017471
Ashutosh Sharma <52165511+Ashu2195@users.noreply.github.com>
2024-11-09 16:34:21 +0530

    Delete Fig2 directory

--- Fig2/Fig2_A/Fig2_A.sh
+++
@@ -1,6 +0,0 @@
-bgzip -c  ID_locus.recode.vcf >  ID_locus.recode.vcf.gz
-tabix -p vcf  ID_locus.recode.vcf.vcf.gz
-
-#phylogeny 
-vk phylo tree nj  ID_locus.recode.vcf.vcf.gz >ID_locus_vkphylo_NJ.nwk
-vk phylo tree upgma  ID_locus.recode.vcf.vcf.gz >ID_locus_vkphylo_UPGMA.nwk
--- Fig2/Fig2_B/Fig2_B.sh
+++
@@ -1,4 +0,0 @@
-angsd -GL 2 -out genolike_ID_78_80 -nThreads 32 -minMapQ 30 -minQ 20 -doGlf 2 -doMajorMinor 1 -SNP_pval 1e-6 -doMaf 1 -bam 270_ind_bam_list -r Z:78725000-80340000
-
-ls -1 *beagle.gz > list_beagle
-for i in `cat list_beagle`; do python pcangsd.py -beagle $i -out $i.cov -threads 64; done
